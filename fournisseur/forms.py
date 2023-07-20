48c9f71b2d4c6618912811d565fefe05ca51cd38
Anouar Ait Abdelmalek <aaitabdelmalek@webpick.net>
2023-07-20 09:54:43 +0000

    Revert "Merge branch 'dev_initial_commit' into 'main'"
    This reverts merge request !2

--- fournisseur/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Fournisseur
-
-class FournisseurForm(forms.ModelForm):
-    class Meta:
-        model = Fournisseur
-        fields = ['nomFournisseur', 'adresse', 'email', 'telephone']
--- materiels/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaterielsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'materiels'
